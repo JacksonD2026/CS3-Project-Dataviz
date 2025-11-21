ed8918e8d236eb79313ca1a83367ee0b102ada3c
jackson <jackson.dobson2026@gbwl.org>
2025-11-21 14:52:04 +0000

    garph

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ plt.style.use('seaborn-v0_8-pastel')
 
 
 df = pd.read_csv('ClassDataset2025 - class-dataset.csv')
+df = df.head(8)
 print(df.info())
 
 birthYear = df['Birth Year']
@@ -13,6 +14,13 @@ birthDay = df['Birth Day']
 
 plt.scatter(birthYear, birthDay)
 
+plt.title('Birth Year VS Birth Day')
+plt.xlabel('Birth Year')
+plt.ylabel('Birth Day')
+plt.axis('equal')
+
 plt.savefig('scatter.png')
 plt.close()
+
+
 
